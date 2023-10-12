c56929d246d37182f0d89f5766f5620f3e73fb93
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-11 21:28:06 -0700

    Initial

---
+++ file/utils.go
@@ -0,0 +1,15 @@
+package file
+
+import (
+	"os"
+)
+
+func checkFileExists(path string) bool {
+	if _, err := os.Stat(path); err == nil {
+		return true
+	} else if os.IsNotExist(err) {
+		return false
+	} else {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/taylormonacelli/forestfish
+
+go 1.21.1
