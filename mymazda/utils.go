f523bb46645b2bf24505509d395c70877be24df4
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-15 07:34:51 -0700

    Add file io that does defer properly

--- mymazda/utils.go
+++ mymazda/utils.go
@@ -1,6 +1,7 @@
 package file
 
 import (
+	"log/slog"
 	"os"
 )
 
@@ -11,5 +12,24 @@ func FileExists(path string) bool {
 		return false
 	} else {
 		panic(err)
+	}
+}
+
+func CreateFile(p string) *os.File {
+	// https://gobyexample.com/defer
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+
+	return f
+}
+
+func CloseFile(f *os.File) {
+	// https://gobyexample.com/defer
+	err := f.Close()
+	if err != nil {
+		slog.Error("file close", "error", err.Error())
+		os.Exit(1)
 	}
 }
